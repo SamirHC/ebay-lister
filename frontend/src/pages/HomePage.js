b981a9d59ce77eb69b8e5a0dafa177914dc6bbd3
Samir Hasan Chowdhury <shc220@ic.ac.uk>
2024-06-23 18:07:06 +0100

    production code

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const HomePage = () => {
     function handleLogin() {
-        const clientId = 'SamirCho-itemtrac-SBX-e4da79f8c-6c999886';
-        const redirectUri = 'Samir_Chowdhury-SamirCho-itemtr-hdghgnj';
+        const clientId = 'SamirCho-itemtrac-PRD-c4db2d93b-dd561dd4';
+        const redirectUri = 'Samir_Chowdhury-SamirCho-itemtr-apwqznrun'  // 'Samir_Chowdhury-SamirCho-itemtr-hdghgnj';
         const scope = 'https://api.ebay.com/oauth/api_scope/sell.inventory';
-        const authUrl = `https://auth.sandbox.ebay.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
+        const authUrl = `https://auth.ebay.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
         
         window.location.href = authUrl;
       }
